6827caf82854480a1a2f972c2378b4414e12570e
Erick Marques <erick.andrade@mv.com.br>
2024-07-30 23:32:30 -0300

    Removendo a data de criação do record e colocando lógica na entidade.

--- src/main/java/com/erickmarques/notify_hub/controller/dto/CreateNotificationDto.java
+++ src/main/java/com/erickmarques/notify_hub/controller/dto/CreateNotificationDto.java
@@ -2,22 +2,19 @@ package com.erickmarques.notify_hub.controller.dto;
 
 import com.erickmarques.notify_hub.entity.Notification;
 
-import java.time.LocalDateTime;
 
-public record CreateNotificationDto (LocalDateTime dateTime,
-                                     String destination,
+
+public record CreateNotificationDto (String destination,
                                      String message,
                                      String channel){
 
     public Notification toNotification() {
         return Notification
                 .builder()
-                .dateTime(dateTime)
                 .destination(destination)
                 .message(message)
                 .status(null)
                 .channel(null)
                 .build();
     }
-
 }
