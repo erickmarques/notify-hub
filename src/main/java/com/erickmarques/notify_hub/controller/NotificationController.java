0f23eb82b1a64c06e4c3efe12df4167fb1504e29
Erick Marques <erick.andrade@mv.com.br>
2024-07-31 10:24:10 -0300

    Adicionando validação para não permitir campos da notificação vazio ou nulo.

--- src/main/java/com/erickmarques/notify_hub/controller/NotificationController.java
+++ src/main/java/com/erickmarques/notify_hub/controller/NotificationController.java
@@ -3,6 +3,7 @@ package com.erickmarques.notify_hub.controller;
 import com.erickmarques.notify_hub.controller.dto.NotificationCreateDto;
 import com.erickmarques.notify_hub.controller.dto.NotificationResponseDto;
 import com.erickmarques.notify_hub.service.NotificationService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -16,7 +17,7 @@ public class NotificationController {
     private final NotificationService notificationService;
 
     @PostMapping
-    public ResponseEntity<String> create(@RequestBody NotificationCreateDto notificationCreateDto) {
+    public ResponseEntity<String> create(@Valid @RequestBody NotificationCreateDto notificationCreateDto) {
         var id = notificationService.create(notificationCreateDto);
         return ResponseEntity.status(HttpStatus.CREATED).body(id);
     }
--- src/main/java/com/erickmarques/notify_hub/controller/dto/NotificationCreateDto.java
+++ src/main/java/com/erickmarques/notify_hub/controller/dto/NotificationCreateDto.java
@@ -3,11 +3,12 @@ package com.erickmarques.notify_hub.controller.dto;
 import com.erickmarques.notify_hub.entity.Channel;
 import com.erickmarques.notify_hub.entity.Notification;
 import com.erickmarques.notify_hub.entity.Status;
+import jakarta.validation.constraints.NotBlank;
 
 
-public record NotificationCreateDto(String destination,
-                                    String message,
-                                    String channel){
+public record NotificationCreateDto(@NotBlank(message = "Favor informar o destino!") String destination,
+                                    @NotBlank(message = "Favor informar a mensagem!") String message,
+                                    @NotBlank(message = "Favor informar o canal!") String channel){
 
     public Notification toNotification(Channel channel) {
         return Notification
