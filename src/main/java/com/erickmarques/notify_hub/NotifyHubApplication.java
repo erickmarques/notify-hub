e1d87deadd7b6535492b1d7314e9cc8faf18bda8
Erick Marques <erick.andrade@mv.com.br>
2024-07-30 22:50:25 -0300

    Iniciando projeto de notificação.

---
+++ src/main/java/com/erickmarques/notify_hub/NotifyHubApplication.java
@@ -0,0 +1,13 @@
+package com.erickmarques.notify_hub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotifyHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotifyHubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/erickmarques/notify_hub/NotifyHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.erickmarques.notify_hub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotifyHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
