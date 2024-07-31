45beaf72057d550e8eba820b10ac07267932e427
Erick Marques <erick.andrade@mv.com.br>
2024-07-31 00:08:53 -0300

    Habilitando o JPA auditing.

--- src/main/java/com/erickmarques/notify_hub/NotifyHubApplication.java
+++ src/main/java/com/erickmarques/notify_hub/NotifyHubApplication.java
@@ -2,8 +2,10 @@ package com.erickmarques.notify_hub;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class NotifyHubApplication {
 
 	public static void main(String[] args) {
