670f2627b5f4c885b1a17d07ebf91a1e50405320
Erick Marques <erick.andrade@mv.com.br>
2024-07-30 23:32:01 -0300

    Criando getters do DTO de channel.

--- src/main/java/com/erickmarques/notify_hub/entity/Channel.java
+++ src/main/java/com/erickmarques/notify_hub/entity/Channel.java
@@ -3,9 +3,11 @@ package com.erickmarques.notify_hub.entity;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.Getter;
 
 @Entity
 @Table(name = "channels")
+@Getter
 public class Channel {
     @Id
     private Long id;
