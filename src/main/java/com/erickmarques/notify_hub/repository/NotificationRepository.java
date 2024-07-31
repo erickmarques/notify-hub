4b513554c4cfcafe107a54d0c420ae62a01ffcc3
Erick Marques <erick.andrade@mv.com.br>
2024-07-31 10:19:51 -0300

    Criando query para pesquisar de notificações que ainda não foram enviadas.

--- src/main/java/com/erickmarques/notify_hub/repository/NotificationRepository.java
+++ src/main/java/com/erickmarques/notify_hub/repository/NotificationRepository.java
@@ -1,9 +1,17 @@
 package com.erickmarques.notify_hub.repository;
 
 import com.erickmarques.notify_hub.entity.Notification;
+import com.erickmarques.notify_hub.entity.Status;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.time.LocalDateTime;
+import java.util.List;
 import java.util.UUID;
 
 public interface NotificationRepository extends JpaRepository<Notification, UUID> {
+
+    @Query("SELECT n FROM Notification n WHERE n.status IN :status AND n.dateTime < :now")
+    List<Notification> findByStatusInAndDateTimeBeforeNow(@Param("status") List<Status> status, @Param("now") LocalDateTime now);
 }
