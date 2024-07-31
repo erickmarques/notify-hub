e5f90ba1f0846668c269a391cbe7648b33b659ea
Erick Marques <erick.andrade@mv.com.br>
2024-07-31 10:20:16 -0300

    Criando serviço de scheduler para envios de notificações em 1 em 1 minuto.

--- src/main/java/com/erickmarques/notify_hub/scheduler/NotificationScheduler.java
+++ src/main/java/com/erickmarques/notify_hub/scheduler/NotificationScheduler.java
@@ -1,4 +1,26 @@
 package com.erickmarques.notify_hub.scheduler;
 
+import com.erickmarques.notify_hub.service.NotificationService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.time.LocalDateTime;
+import java.util.concurrent.TimeUnit;
+
+@Component
+@RequiredArgsConstructor
 public class NotificationScheduler {
+
+    private static final Logger logger = LoggerFactory.getLogger(NotificationScheduler.class);
+    private final NotificationService notificationService;
+
+    @Scheduled(fixedDelay = 1, timeUnit = TimeUnit.MINUTES)
+    public void checkNotifications() {
+        var dateTime = LocalDateTime.now();
+        logger.info("Iniciando pesquisa de notificações em {}", dateTime);
+        notificationService.checkAndSend(dateTime);
+    }
 }
